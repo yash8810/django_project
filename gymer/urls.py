13c9443de0caedb9cf5ff77c7fa2f2a1849381c8
yash8810 <yash8810patel@gmail.com>
2025-08-19 15:18:21 +0530

    change in urls.py

--- gymer/urls.py
+++ gymer/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
-
+print("")
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.indexFun, name="homePage"),
