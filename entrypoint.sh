d86564fd81e90402b11f45721389fdf311fe46c7
yash8810 <yash8810patel@gmail.com>
2025-09-09 17:50:06 +0530

    changes in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,9 +25,9 @@ import os
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
-username = os.getenv("DJANGO_SUPERUSER_USERNAME", "admin")
-email = os.getenv("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
-password = os.getenv("DJANGO_SUPERUSER_PASSWORD", "adminpass")
+username = os.getenv("DJANGO_SUPERUSER_USERNAME") or "admin"
+email = os.getenv("DJANGO_SUPERUSER_EMAIL") or "admin@example.com"
+password = os.getenv("DJANGO_SUPERUSER_PASSWORD") or "adminpass"
 
 if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(username=username, email=email, password=password)
